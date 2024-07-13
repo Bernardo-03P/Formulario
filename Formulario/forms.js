5d12745579ebf3eb36f33b6848c7ef8c3f82bac2
Bernardo Pozzebon Poluceno <162700391+Bernardo-03P@users.noreply.github.com>
2024-07-13 14:50:47 -0300

    Pronto

--- Formulario/forms.js
+++ Formulario/forms.js
@@ -6,12 +6,30 @@ function Enviar(){
     let carros = document.querySelector("input[name='carro']:checked").value
     let motos = document.querySelector("input[name='moto']:checked").value
 
+    let jesko = document.getElementById("Jesko")
+    let laferrari = document.getElementById("Laferrari")
+    let camaro = document.getElementById("SS 69")
+    let dodge = document.getElementById("Demon")
+    let sportster = document.getElementById("Sportster S")
+    let fatbob = document.getElementById("Fat Bob")
+    let cbr1000 = document.getElementById("CBR 1000")
+    let zxr = document.getElementById("ZX-10R")
 
-    alert(`Seu nome é: ${Nomeuser}\n
-           Seu email é: ${Emailuser}\n
-           Sua senha é: ${Senhauser}\n
-           Seu modelo de carro escolhido é: ${carros}\n
-           Seu modelo de moto escolhido é: ${motos}`)
+if(jesko.checked || laferrari.checked || camaro.checked || dodge.checked || sportster.checked || fatbob.checked || cbr1000.checked || zxr.checked){ 
+    
 
-           window.location.href=`tela2.html`
+        
+        alert(`Seu nome é: ${Nomeuser}\n
+            Seu email é: ${Emailuser}\n
+            Sua senha é: ${Senhauser}\n
+            Seu modelo de carro escolhido é: ${carros}\n
+            Seu modelo de moto escolhido é: ${motos}`)
+
+        
+            window.location.href=`tela2.html`
+}else{
+
+    alert(`Escolha alguma das opções!`)
+}
+
 }
